ffb1c3b7569c513ebdffa972ecd6c9d277fe105c
developer@lunarhook.com <801105Fxl@>
2020-07-20 13:24:02 +0800

    Initial commit

---
+++ go.mod.go
@@ -0,0 +1,25 @@
+package AWS_Russia
+
+go 1.14
+
+require (
+github.com/Unknwon/goconfig v0.0.0-20191126170842-860a72fb44fd // indirect
+github.com/aliyun/alibaba-cloud-sdk-go v1.61.13
+github.com/astaxie/beego v1.12.1
+github.com/beego/i18n v0.0.0-20161101132742-e9308947f407
+github.com/dgrijalva/jwt-go v3.2.0+incompatible
+github.com/gorilla/websocket v1.4.1
+github.com/json-iterator/go v1.1.9 // indirect
+github.com/mattn/go-sqlite3 v2.0.3+incompatible
+github.com/modern-go/reflect2 v1.0.1 // indirect
+github.com/russross/blackfriday v2.0.0+incompatible
+github.com/shiena/ansicolor v0.0.0-20151119151921-a422bbe96644 // indirect
+github.com/shurcooL/sanitized_anchor_name v1.0.0 // indirect
+github.com/tidwall/gjson v1.5.0
+github.com/tidwall/pretty v1.0.1 // indirect
+golang.org/x/crypto v0.0.0-20200221231518-2aa609cf4a9d // indirect
+golang.org/x/net v0.0.0-20200226121028-0de0cce0169b // indirect
+google.golang.org/appengine v1.6.5 // indirect
+gopkg.in/ini.v1 v1.52.0 // indirect
+gopkg.in/yaml.v2 v2.2.8 // indirect
+)
---
+++ server/main.go
@@ -0,0 +1,12 @@
+package server
+
+import "C"
+import (
+	"fmt"
+	"github.com/astaxie/beego"
+)
+
+
+func main() {
+	return
+}
