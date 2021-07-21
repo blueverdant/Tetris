ad6e88ad366e6dc8d8aaafc60052523fd3a16d83
fanxiaolong <developer@lunarhook.com>
2021-07-21 15:43:24 +0800

    Update web.sh

--- web.sh
+++ web.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 export PATH=$PATH:/usr/local/go/bin
+git pull
 cd server/Global/main/
 go build -o Russiamain main.go 
 cd ..
@@ -7,5 +8,5 @@ cd ..
 cd ..
 
 ps -ef |grep Russiamain | grep -v grep |awk '{print $2}' | xargs kill -9
-cp -Rf ./server/Global/main/Russiamain ./Russiamain
+#cp -Rf ./server/Global/main/Russiamain ./Russiamain
 nohup ./Russiamain >Russiamain.out 2>&1 &
